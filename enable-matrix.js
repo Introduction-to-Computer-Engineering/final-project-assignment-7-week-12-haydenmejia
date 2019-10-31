3c611cf0b107c400e920c1b96c3145384d1b2679
Hayden Mejia <31870483+haydenmejia@users.noreply.github.com>
2019-10-31 16:00:58 -0600

    Update enable-matrix.js

--- enable-matrix.js
+++ enable-matrix.js
@@ -5,16 +5,16 @@
 basic.showIcon(IconNames.Heart)//displays heart icon on led matrix
 basic.forever(function () {   //function that wraps the leds in a continious cycle
     pins.digitalWritePin(DigitalPin.P0, 1)  //turns on pin 0
-    basic.pause(1000)                       //1 sec on
+    basic.pause(250)                       //.25 sec on
     pins.digitalWritePin(DigitalPin.P0, 0)  //turns off pin 0
-    basic.pause(1000)                       //1 sec off
+    basic.pause(250)                       //.25 sec off
     pins.digitalWritePin(DigitalPin.P1, 1)  //turns on pin 1
-    basic.pause(1000)
+    basic.pause(250)
     pins.digitalWritePin(DigitalPin.P1, 0)  //turns off pin 1 
-    basic.pause(1000)
+    basic.pause(250)
     pins.digitalWritePin(DigitalPin.P2, 1)  //turns on pin 2
-    basic.pause(1000)
+    basic.pause(250)
     pins.digitalWritePin(DigitalPin.P2, 0)  //turns off pin 2
-    basic.pause(1000)
+    basic.pause(250)
 
 })
